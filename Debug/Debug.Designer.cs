99fc638e0cd54ddbdc5c65352b243f57f5b5dc22
Sir丶雨轩 <1718018032@qq.com>
2022-07-19 18:00:04 +0800

    debug

---
+++ Debug/Debug.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace LOLHelper.Debug
+{
+    partial class Debug
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBox_log
+            // 
+            this.txtBox_log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBox_log.Location = new System.Drawing.Point(0, 0);
+            this.txtBox_log.Multiline = true;
+            this.txtBox_log.Name = "txtBox_log";
+            this.txtBox_log.ReadOnly = true;
+            this.txtBox_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBox_log.Size = new System.Drawing.Size(662, 454);
+            this.txtBox_log.TabIndex = 0;
+            // 
+            // Debug
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 453);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtBox_log);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Debug";
+            this.ShowIcon = false;
+            this.Text = "Debug";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Debug_FormClosing);
+            this.Load += new System.EventHandler(this.Debug_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtBox_log;
+    }
+}
---
+++ Debug/Debug.cs
@@ -0,0 +1,82 @@
+﻿using LOLHelper.Const;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOLHelper.Debug
+{
+    public partial class Debug : Form
+    {
+
+        private Form followForm;
+        private readonly System.Timers.Timer timer = new System.Timers.Timer();
+        public Debug(Form followForm)
+        {
+            InitializeComponent();
+            this.followForm = followForm;
+        }
+        /// <summary>
+        /// 写入一行日志
+        /// </summary>
+        /// <param name="str">日志内容</param>
+        public void Log(string str)
+        {
+            string now = DateTime.Now.ToString();
+            if (txtBox_log.InvokeRequired)
+            {
+                Action<string> actionDelegate = (x) =>
+                {
+                    txtBox_log.AppendText(now + ": " + x + "\r\n");
+                };
+                this.txtBox_log.Invoke(actionDelegate, str);
+            }
+            else
+            {
+                txtBox_log.Text += now + ": " + str + "\r\n";
+            }
+        }
+
+        /// <summary>
+        /// 修改窗口位置
+        /// </summary>
+        private void UpdateLocation()
+        {
+            if (this.InvokeRequired)
+            {
+                Action<Form> actionDelegate = (followForm) =>
+                {
+                    Left = followForm.Left + followForm.Width;
+                    Top = followForm.Top;
+                };
+                if (!this.IsDisposed)
+                {
+                    this.Invoke(actionDelegate, followForm);
+                }
+
+            }
+        }
+
+        private void Debug_Load(object sender, EventArgs e)
+        {
+
+            timer.Interval = 100;
+            timer.Elapsed += (s, e1) =>
+            {
+                UpdateLocation();
+            };
+            timer.Start();
+        }
+
+        private void Debug_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer.Stop();
+            timer.Close();
+        }
+    }
+}
