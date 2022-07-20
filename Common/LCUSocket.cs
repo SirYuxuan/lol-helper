7af0175b74ac3758576874079c636f788e07f69d
Sir丶雨轩 <1718018032@qq.com>
2022-07-20 20:29:19 +0800

    init

--- Common/LCUSocket.cs
+++ Common/LCUSocket.cs
@@ -71,7 +71,7 @@ namespace LOLHelper.Common
                                         }
                                         break;
                                     case LCUEvent.ChampSelectUpdateSessionEvt:
-                                        ChampionSelect championSelect = JsonConvert.DeserializeObject<ChampionSelect>(token.ToString());
+                                        。ChampionSelect championSelect = JsonConvert.DeserializeObject<ChampionSelect>(token.ToString());
                                         if (championSelect.eventType.Equals("Update")) 
                                         {
                                           Task<string> task =  LCUApi.GetCurrConversationID();
